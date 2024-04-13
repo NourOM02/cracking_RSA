b5675c0c514301ba6d4b79d2a7269194e0b7ed32
NourOM02 <nourom.professional@gmail.com>
2024-04-13 00:20:46 +0000

    add RSA implementation

---
+++ generate_keys.py
@@ -0,0 +1,76 @@
+import argparse
+from sympy import randprime
+import random
+from math import gcd
+import base64
+
+def generate_prime(bits):
+    min_value = 2 ** (bits - 1)
+    max_value = (2 ** bits) - 1
+    return randprime(min_value, max_value)
+
+def find_e(phi_n):
+    while True:
+      e = random.randint(2, phi_n-1)
+      if gcd(e, phi_n) == 1:
+          return e
+      
+def find_d(e, phi_n):
+    # Using the extended Euclidean algorithm to find the modular inverse
+    def extendedEuclid(a, b):
+        if b == 0:
+            return a, 1, 0
+        else:
+            d2, x2, y2 = extendedEuclid(b, a % b)
+            d, x, y = d2, y2, x2 - (a // b) * y2
+        return d, x, y
+    # The modular inverse of e is the d that satisfies the equation e * d ≡ 1 (mod phi(n))
+    def multiplicativeInverse(e, phi_n):
+        return extendedEuclid(e, phi_n)[1] % phi_n
+    
+    return multiplicativeInverse(e, phi_n)
+
+def generate_keys():
+    # initialize the parser
+    parser = argparse.ArgumentParser(description="A script to generate keys using RSA algorithm.")
+    
+    # add the arguments
+    parser.add_argument('--bits', type=int, default=2048, help='How muh bits the key should have.')
+    
+    # parse the arguments
+    args = parser.parse_args()
+
+    # generate the primes
+    p = generate_prime(args.bits//2)
+    # make sure they are not the same    
+    while True:
+        q = generate_prime(args.bits//2)
+        if p != q:
+            break
+        
+    phi_n = (p - 1) * (q - 1)
+    
+    # determine e and d
+    public_exponent = find_e(phi_n)
+    private_exponent = find_d(public_exponent, phi_n)
+    
+    modulus = p * q
+    
+    public_key = (public_exponent, modulus)
+    private_key = (private_exponent, modulus)
+    
+    # Convert keys to base64 encoding
+    private_key_b64 = base64.b64encode(str(private_key).encode()).decode()
+    public_key_b64 = base64.b64encode(str(public_key).encode()).decode()
+    
+    print("Private key:")
+    print("-----BEGIN RSA PRIVATE KEY-----")
+    print(private_key_b64)
+    print("-----END RSA PRIVATE KEY-----\n")
+    
+    print("Public key:")
+    print("-----BEGIN PUBLIC KEY-----")
+    print(public_key_b64)
+    print("-----END PUBLIC KEY-----")
+
+generate_keys()
